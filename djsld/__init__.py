57e2ba1003bf9ad9aba6fb71beccb540d67c32ea
David Zwarg <dzwarg@azavea.com>
2011-12-08 13:09:53 -0500

    Updated documentation for djsld package. Added epydoc configuration for generating documentation.

---
+++ djsld/__init__.py
@@ -0,0 +1,31 @@
+"""
+djsld, or django-sld, is a library that generates cartographic representations
+in the form of SLD XML files from data distributions in the form of django 
+querysets.
+
+The module L{djsld.generator} contains all methods to generate different types
+of classifications. The classification algorithms are implemented in the pysal
+library.
+
+License
+=======
+Copyright 2011 David Zwarg <U{dzwarg@azavea.com}>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+U{http://www.apache.org/licenses/LICENSE-2.0}
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: David Zwarg
+@contact: dzwarg@azavea.com
+@copyright: 2011, Azavea
+@license: Apache 2.0
+@version: 1.0.0
+"""
